64b516ab5a19a1e63beba70a1d58d2455c79e3c2
Nandy Bâ <nandyba@yahoo.fr>
2023-10-21 22:23:15 +0200

    Add IDE basic

--- front/src/pages/Quest/Quest.js
+++ front/src/pages/Quest/Quest.js
@@ -1,11 +1,43 @@
+import React, { useRef, useEffect } from 'react';
+import MonacoEditor from 'react-monaco-editor';
 import styles from './Quest.module.css';
 
 const Quest = ({ title, description, reward }) => {
+    const editorRef = useRef(null);
+
+    const editorOptions = {
+        selectOnLineNumbers: true,
+        roundedSelection: false,
+        readOnly: false,
+        cursorStyle: 'line',
+        automaticLayout: true,
+    };
+
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className={styles.questContainer}>
+            <h1>{title}</h1>
             <p>{description}</p>
-            <p>Reward: {reward}</p>
+            <div className={styles.reward}>
+                <p>Reward: {reward}</p>
+            </div>
+            <div className={styles.IDE}>
+                <MonacoEditor
+                    ref={editorRef}
+                    height="600"
+                    language="javascript"
+                    theme="vs-dark"
+                    options={editorOptions}
+                    onChange={(newValue, e) => {
+                        // handle the change of the editor's content
+                        console.log('onChange', newValue, e);
+                    }}
+                    editorDidMount={(editor, monaco) => {
+                        // here is the editor's instance, and you get access to the monaco instance as well
+                        console.log('editorDidMount', editor);
+                        editorRef.current = editor; // Storing editor instance
+                    }}
+                />
+            </div>
         </div>
     );
 };
