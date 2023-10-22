a9825b417a4501e37ff85a610447680053c7ef71
Nandy Bâ <nandyba@yahoo.fr>
2023-10-22 08:26:33 +0200

    Add new home

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import Page404 from './pages/404/404';
 import Quest from './pages/Quest/Quest';
+import NewHome from './pages/NewHome/NewHome';
 
 function App({ code }) {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/newhome" element={<NewHome />} />
         <Route path='/quest' element={<Quest title="Quest Title" description="Quest Description" reward="Quest Reward" />} />
         <Route path="404" element={<Page404 />} />
         <Route path="*" element={<Navigate to="/404" />} />
